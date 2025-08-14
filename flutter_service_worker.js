70c52fcce86ec970f5b4c65259fb9ebd315c9af4
HalilALPAK <halil10alpak@gmail.com>
2025-08-14 19:04:42 +0300

    Fix base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e4d57616ca8f53898ed2b85c29d86802
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "12d4834636f17b8b469572b3b330625c",
+"flutter_bootstrap.js": "e95173b20426b874ac65f0a7808c1d53",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
